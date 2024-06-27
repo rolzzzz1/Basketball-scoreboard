e26eb258f38a5d218eb4d2b50bba734f0d281798
Aishwarya Sharma <roli.mradul@gmail.com>
2024-06-27 10:23:29 +0530

    After review correction

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-let homeEl = document.getElementById("home-el")
-let guestEl = document.getElementById("guest-el")
-let homeCount = 0
-let guestCount = 0
+const homeEl = document.getElementById("home-el");
+const guestEl = document.getElementById("guest-el");
+let homeCount = 0;
+let guestCount = 0;
 
-function homePlus1() {
-    homeCount += 1
-    homeEl.textContent = homeCount;
-    console.log("Home +1 => " + homeCount)
+function homePlus(points) {
+  homeCount += points;
+  homeEl.textContent = homeCount;
 }
 
-function homePlus2() {
-    homeCount += 2
-    homeEl.textContent = homeCount;
-    console.log("Home +2 => " + homeCount)
+function guestPlus(points) {
+  homeCount += points;
+  guestEl.textContent = homeCount;
 }
 
-function homePlus3() {
-    homeCount += 3
-    homeEl.textContent = homeCount;
-    console.log("Home +2 => " + homeCount)
-}
+// function homePlus1() {
+//     homeCount += 1
+//     homeEl.textContent = homeCount;
+//     console.log("Home +1 => " + homeCount)
+// }
 
-function guestPlus1() {
-    guestCount += 1
-    guestEl.textContent = guestCount;
-    console.log("Guest +1 => " + guestCount)
-}
+// function homePlus2() {
+//     homeCount += 2
+//     homeEl.textContent = homeCount;
+//     console.log("Home +2 => " + homeCount)
+// }
 
-function guestPlus2() {
-    guestCount += 2
-    guestEl.textContent = guestCount;
-    console.log("Guest +2 => " + guestCount)
-}
+// function homePlus3() {
+//     homeCount += 3
+//     homeEl.textContent = homeCount;
+//     console.log("Home +2 => " + homeCount)
+// }
 
-function guestPlus3() {
-    guestCount += 3
-    guestEl.textContent = guestCount;
-    console.log("Guest +3 => " + guestCount)
-}
+// function guestPlus1() {
+//     guestCount += 1
+//     guestEl.textContent = guestCount;
+//     console.log("Guest +1 => " + guestCount)
+// }
+
+// function guestPlus2() {
+//     guestCount += 2
+//     guestEl.textContent = guestCount;
+//     console.log("Guest +2 => " + guestCount)
+// }
+
+// function guestPlus3() {
+//     guestCount += 3
+//     guestEl.textContent = guestCount;
+//     console.log("Guest +3 => " + guestCount)
+// }
